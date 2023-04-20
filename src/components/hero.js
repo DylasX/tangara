f3e7f5e927ef9682b3bacc6c6b08d6f0dab7b53b
Miguel <miguel-sg01@hotmail.com>
2023-04-19 21:33:49 -0500

    update features

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { GatsbyImage } from 'gatsby-plugin-image'
 import { renderRichText } from 'gatsby-source-contentful/rich-text'
+import { StaticImage } from 'gatsby-plugin-image'
 
 import * as styles from './hero.module.css'
 
@@ -10,9 +11,18 @@ const Hero = ({ image, title, content }) => (
       <GatsbyImage className={styles.image} alt={title} image={image} />
     )}
     <div className={styles.details}>
-      <h1 className={styles.title}>{title}</h1>
+      <StaticImage
+        className={styles.title}
+        alt="logo"
+        src="../../static/images/logo.jpg"
+        width={'100px'}
+        height={'100px'}
+      />{' '}
       {content && (
-        <div className={styles.content}>{renderRichText(content)}</div>
+        <>
+          <div className={styles.content}>{renderRichText(content)}</div>
+          <button className="btn">Reservar</button>
+        </>
       )}
     </div>
   </div>
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -2,22 +2,27 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import * as styles from './navigation.module.css'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const Navigation = () => (
   <nav role="navigation" className={styles.container} aria-label="Main">
     <Link to="/" className={styles.logoLink}>
-      <span className={styles.logo} />
-      <span className={styles.navigationItem}>Gatsby Starter Contentful</span>
+      <StaticImage
+        className={styles.logo}
+        alt="logo"
+        src="../../static/images/logo.jpg"
+      />
+      <span className={styles.navigationItem}>Tangara</span>
     </Link>
     <ul className={styles.navigation}>
       <li className={styles.navigationItem}>
         <Link to="/" activeClassName="active">
-          Home
+          Inicio
         </Link>
       </li>
       <li className={styles.navigationItem}>
         <Link to="/blog/" activeClassName="active">
-          Blog
+          Reservar
         </Link>
       </li>
     </ul>
